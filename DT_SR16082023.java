95ae20201a8da81e5f171676dac17bbce55d86a2
a-shok27 <ashokalagirisamy276@gmail.com>
2023-08-17 09:29:24 +0530

    Daily test On 16/08/2023 in Skillrack submitted

---
+++ DT_SR16082023.java
@@ -0,0 +1,17 @@
+package Java;
+
+import java.util.Scanner;
+
+public class DT_SR16082023 {
+    public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n/2;i++){
+            if((Math.pow(2,i)+1)==n||(Math.pow(2,i)-1)==n){
+                System.out.print(i);
+                return ;
+            }
+        }
+        System.out.print("No");
+    }
+}

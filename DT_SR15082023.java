66fc3c6c920d22eeb0b5d86994e3f3f039294f13
a-shok27 <ashokalagirisamy276@gmail.com>
2023-08-17 09:33:56 +0530

    updated

--- DT_SR15082023.java
+++ DT_SR15082023.java
@@ -12,29 +12,30 @@ public class DT_SR15082023 {
         }
     }
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
-        int c=0;
-        for(int i=0;i<s.length();i++){
-            if(isVowel(s.charAt(i))){
-                c++;
-            }
-        }
-        int z1=1;
-        int z=0;
-        for(int i=0;i<c;i++){
-            for(int j=0;j<s.length();j++){
-                if(isVowel(s.charAt(j)) && z!=z1){
-                    System.out.print("["+s.charAt(j)+"]");
-                    z++;
-                }
-                else{
-                    System.out.print(s.charAt(j));
+        try (Scanner sc = new Scanner(System.in)) {
+            String s=sc.nextLine();
+            int c=0;
+            for(int i=0;i<s.length();i++){
+                if(isVowel(s.charAt(i))){
+                    c++;
                 }
             }
-            System.out.println();
-            z=0;
-            z1++;
+            int z1=1;
+            int z=0;
+            for(int i=0;i<c;i++){
+                for(int j=0;j<s.length();j++){
+                    if(isVowel(s.charAt(j)) && z!=z1){
+                        System.out.print("["+s.charAt(j)+"]");
+                        z++;
+                    }
+                    else{
+                        System.out.print(s.charAt(j));
+                    }
+                }
+                System.out.println();
+                z=0;
+                z1++;
+            }
         }
     }
 }
--- DT_SR16082023.java
+++ DT_SR16082023.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class DT_SR16082023 {
     public static void main(String[]args){
-    Scanner sc=new Scanner(System.in);
+    try (Scanner sc = new Scanner(System.in)) {
         int n=sc.nextInt();
         for(int i=1;i<=n/2;i++){
             if((Math.pow(2,i)+1)==n||(Math.pow(2,i)-1)==n){
@@ -12,6 +12,7 @@ public class DT_SR16082023 {
                 return ;
             }
         }
+    }
         System.out.print("No");
     }
 }
